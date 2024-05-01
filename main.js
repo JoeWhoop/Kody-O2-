9a460259c07a78ad968c420c809d0b90d4b38d9c
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2024-05-01 20:19:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,17 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
         var stationValue = document.getElementById('stationDropdown').value;
         var mechanicValue = document.getElementById('mechanicDropdown').value;
 
-        // Pobierz link
-        var link = document.getElementById('link');
-
-        // Dodaj wartość mechanic do linku
-        var selectedMechanic = encodeURIComponent(mechanicValue);
-        var linkURL = "https://web.miniextensions.com/Vp81zD5sRnZVQZ2lAgLJ?prefill_Mechanic=" + selectedMechanic;
-
-        // Jeśli wybrano D w dropdownie dla stacji, użyj linku
+        // Sprawdź, czy wybrano stację "D"
         if (stationValue === "D") {
-            link.href = linkURL;
-            link.click();
+            // Pobierz link
+            var link = document.createElement('a');
+            link.href = "https://web.miniextensions.com/Vp81zD5sRnZVQZ2lAgLJ";
+
+            // Dodaj wartość mechanic do linku
+            var selectedMechanic = encodeURIComponent(mechanicValue);
+            link.href += "?prefill_Mechanic=" + selectedMechanic;
+
+            // Otwórz link
+            link.target = "_blank"; // Otwórz w nowym oknie
+            document.body.appendChild(link); // Dodaj link do body
+            link.click(); // Kliknij link
+            document.body.removeChild(link); // Usuń link z body
+        } else {
+            // Komunikat dla użytkownika, gdy wybrano inną stację
+            alert("Wybierz stację D, aby kontynuować.");
         }
     });
 });
