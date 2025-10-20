88c43bb590b42fd6df85138542bdbd0612cfd00c
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2025-10-20 21:56:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ document.getElementById("goButton").addEventListener("click", function () {
     var baseRedirectURL = "https://web.miniextensions.com/Vp81zD5sRnZVQZ2lAgLJ?prefill_Mechanic=" + selectedMechanic;
 
     var redirectionURLs = {
+        "D-1": baseRedirectURL + "&prefill_Station=" + selectedStation,
         "D0": baseRedirectURL + "&prefill_Station=" + selectedStation,
         "D1": baseRedirectURL + "&prefill_Station=" + selectedStation,
         "D2": baseRedirectURL + "&prefill_Station=" + selectedStation,
